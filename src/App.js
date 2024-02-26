48980f59dab168a23b3eb39a16f471aa9388054c
Prapti Maheshwari <praptimaheshwari@Praptis-MacBook-Air.local>
2024-02-26 17:05:30 +0530

    added  sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Sidebar/>
       {/* Other content of your application */}
     </div>
   );
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Sidebar.css'; // Import CSS file for styling
+import logo from '../assets/sidebarlogo.jpeg';
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-header">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className="firstrow">
+
+            </div>
+            <ul className="sidebar-menu">
+                <li ><a href="#">BBS Home</a></li>
+                <li><span className="triangle"></span><a href="#">Create Request</a></li>
+                <li><a href="#">Review Request</a></li>
+                <li><a href="#">Overview</a></li>
+                <li><a href="#">History of authorization</a></li>
+                <li><span className="triangle"></span><a href="#">Information on</a></li>
+                <li><span className="triangle"></span><a href="#">Adminstration</a></li>
+                <li><span className="triangle"></span><a href="#">Resource admin</a></li>
+                <li><span className="triangle"></span><a href="#">Resource access</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
+
